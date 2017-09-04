8bf17ce1f85e2d032fbf4bd2dfd180c4b02f96f8
hlovatt <howard.lovatt@gmail.com>
2017-09-04 15:24:55 +1000

    Initial Commit

---
+++ Concurrency Utilities/Concurrency_Utilities.h
@@ -0,0 +1,19 @@
+//
+//  Concurrency_Utilities.h
+//  Concurrency Utilities
+//
+//  Created by Howard Lovatt on 4/9/17.
+//  Copyright © 2017 RHJOD. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Concurrency_Utilities.
+FOUNDATION_EXPORT double Concurrency_UtilitiesVersionNumber;
+
+//! Project version string for Concurrency_Utilities.
+FOUNDATION_EXPORT const unsigned char Concurrency_UtilitiesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Concurrency_Utilities/PublicHeader.h>
+
+
