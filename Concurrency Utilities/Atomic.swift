33a6b00baf1cced6718efa9397b8c7f15f3ec512
hlovatt <howard.lovatt@gmail.com>
2017-09-04 15:51:20 +1000

    Initial commit

---
+++ Concurrency Utilities/Atomic.swift
@@ -0,0 +1,54 @@
+//
+//  Atomic.swift
+//  Concurrency Utilities
+//
+//  Created by Howard Lovatt on 4/9/17.
+//  Copyright © 2017 Howard Lovatt. Creative Commons Attribution 4.0 International License.
+//
+
+import Foundation
+
+/// Gives atomic get/set/update to its value.
+public final class Atomic<T> {
+    private var queue: DispatchQueue!
+    
+    private var _value: T
+    
+    /// Creat and initialize a new atomic.
+    /// - parameter initialValue: The initial value of the variable.
+    init(_ initialValue: T) {
+        _value = initialValue
+        queue = DispatchQueue(label: "Atomic Serial Queue \(ObjectIdentifier(self))", qos: DispatchQoS.userInitiated)
+    }
+    
+    /// Atomically get and set the value.
+    /// - note: See `update` for getting and then setting the value atomically.
+    var value: T {
+        get {
+            var value: T?
+            queue.sync {
+                value = self._value
+            }
+            return value!
+        }
+        set {
+            queue.sync {
+                self._value = newValue
+            }
+        }
+    }
+    
+    /// Atomically update the value (get then set value in one operation guaranteeing no `get`s, `set`s, or `update`s from other threads inbetween).
+    /// - parameters:
+    ///   - updater: A closure that accepts the current value and returns the new value.
+    ///   - oldValue: The old value supplied to given updater.
+    /// - returns: The updatded value (which can be ignored).
+    @discardableResult func update(updater: (_ oldValue: T) -> T) -> T {
+        var result: T? = nil
+        queue.sync {
+            result = updater(self._value)
+            self._value = result!
+        }
+        return result!
+    }
+}
